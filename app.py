1d38eb7c33e146ec03d8181a20213717d735f965
Khaled Shehada <sheahdak@mit.edu>
2019-06-24 16:51:57 -0400

    new modification

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, redirect, url_for
-app = Flask(__name__)
+# from flask import Flask, render_template, redirect, url_for
+# app = Flask(__name__)
 
 
-@app.route('/home')
-def HomePage():
-    return render_template("index.html")
+# @app.route('/home')
+# def HomePage():
+#     return render_template("index.html")
 
-@app.route('/')
-def redirect_to_HomePage():
-	return (redirect(url_for('HomePage')))
+# @app.route('/')
+# def redirect_to_HomePage():
+# 	return (redirect(url_for('HomePage')))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def is_power_of_2(n):
+	if n%2==1:
+		return False
+	elif n==2:
+		return True
+	else:
+		return is_power_of_2(n/2)
+
+
+print(is_power_of_2(18))
+# if __name__ == '__main__':
+#     app.run(debug=True)
